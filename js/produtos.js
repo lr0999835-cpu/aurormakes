4730d6f95b4fa00d22349428b6ff3137da59c1ad
lr0999835-cpu <lr0999835@gmail.com>
2026-03-13 22:39:00 -0300

    Transform site into makeup store with cart and WhatsApp checkout

---
+++ js/produtos.js
@@ -0,0 +1,38 @@
+const CATALOGO_PRODUTOS = [
+  {
+    id: 1,
+    nome: "Base Líquida Glow",
+    preco: 59.9,
+    imagem: "images/base-glow.svg"
+  },
+  {
+    id: 2,
+    nome: "Paleta Aurora 12 Cores",
+    preco: 89.9,
+    imagem: "images/paleta-aurora.svg"
+  },
+  {
+    id: 3,
+    nome: "Batom Matte Rosé",
+    preco: 34.9,
+    imagem: "images/batom-rose.svg"
+  },
+  {
+    id: 4,
+    nome: "Máscara de Cílios Volume",
+    preco: 42.9,
+    imagem: "images/mascara-cilios.svg"
+  },
+  {
+    id: 5,
+    nome: "Iluminador Crystal",
+    preco: 39.9,
+    imagem: "images/iluminador.svg"
+  },
+  {
+    id: 6,
+    nome: "Blush Peach Shine",
+    preco: 36.9,
+    imagem: "images/blush-peach.svg"
+  }
+];
